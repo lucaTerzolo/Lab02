cf387a4a65cda1c6ecd218021ccdb2b26c507b13
luca <luca@localhost>
2022-03-16 17:13:56 +0100

    Es. 1 fatto con mod VCM

--- src/main/java/it/polito/tdp/alien/FXMLController.java
+++ src/main/java/it/polito/tdp/alien/FXMLController.java
@@ -6,9 +6,16 @@ package it.polito.tdp.alien;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.alien.model.Model;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
+	
+	private Model model;
 
     @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
@@ -16,9 +23,41 @@ public class FXMLController {
     @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
 
+    @FXML // fx:id="txtTradotto"
+    private TextArea txtTradotto; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtTraduci"
+    private TextField txtTraduci; // Value injected by FXMLLoader
+
+    @FXML
+    void handleCleatText(ActionEvent event) {
+    	txtTradotto.clear();
+    }
+
+    @FXML
+    void handleTranslate(ActionEvent event) throws Exception {
+    	String s=txtTraduci.getText();
+    	String stampa=new String();
+    	String[] parole=s.toLowerCase().split(" ");
+    	if(parole.length==1) {
+    		stampa=this.model.traduci(parole[0]);
+    	}
+    	if(parole.length==2) {
+    		stampa=this.model.aggiungi(parole[0],parole[1]);
+    	}
+    	txtTradotto.appendText(stampa+"\n");
+    	txtTraduci.clear();
+    }
+
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
+        assert txtTradotto != null : "fx:id=\"txtTradotto\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtTraduci != null : "fx:id=\"txtTraduci\" was not injected: check your FXML file 'Scene.fxml'.";
 
     }
 
+    public void setModel(Model model) {
+    	this.model=model;
+    }
+    
 }
---
+++ src/main/java/it/polito/tdp/alien/model/Model.java
@@ -0,0 +1,54 @@
+package it.polito.tdp.alien.model;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Model {
+	
+	private Map<String,String> dizionario;
+	//Italiano alieno
+	
+	public Model() {
+		this.dizionario = new TreeMap<>();
+	}
+	
+	public String traduci(String string) {
+		String s=new String();
+		String it=string;
+		if(!valido(string)) {
+			s="Erore di inserimento";
+			return s;
+		}
+		if(!dizionario.containsKey(it))
+			s="Non trovato";
+		else
+			s=dizionario.get(it);
+		return s;
+	}
+
+	public String aggiungi(String string, String string2) {
+		String s=new String();
+		if(!valido(string)||!valido(string2)) {
+			s="Erore di inserimento";
+			return s;
+		}
+		if(dizionario.containsKey(string))
+			s="GIA INSERITO";
+		else
+			dizionario.put(string, string2);
+		return s;
+	}
+	
+	public boolean valido(String string) {
+		string.toCharArray();
+		boolean prova=false;
+		for(int i=0;i<string.length();i++) {
+			if(Character.isAlphabetic(string.charAt(i)))
+				prova=true;
+			else
+				prova=false;
+		
+		}
+		return prova;
+	}
+}	
